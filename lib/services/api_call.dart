eb9ce3f70bc12e375adcfbac5787a2a063b79569
os <kkeval304@gmail.com>
2024-02-17 09:51:14 +0530

    new

--- lib/services/api_call.dart
+++ lib/services/api_call.dart
@@ -6,25 +6,25 @@ import '../models/reg.dart';
 
 class Apicall {
   static String apiUrl =
-      "http://192.168.43.213/seminar%20booking%20system/php_application/lib/";
+      "http://192.168.135.86/seminar%20booking%20system/php_application/lib/";
   static String imgUrl =
-      "http://192.168.43.213/seminar%20booking%20system/php_application/images/upload/";
+      "http://192.168.135.86/seminar%20booking%20system/php_application/images/upload/";
 
   Future<Registration> createRegistration(Registration registration) async {
-    // Map data = {
-    //   // 'u_id': '0' ,
-    //   // 'u_profile': registration.first_name,
-    //   'u_name': registration.u_name,
-    //   'u_email': registration.u_email,
-    //   'u_contactno': registration.u_contactno,
-    //   'u_psw': registration.u_psw,
-    //   // 'u_address':registration.address,
-    //   // 'country':registration.country,
-    //   // 'state':registration.state,
-    //   // 'city':registration.city,
-    // };
+    Map data = {
+      // 'u_id': '0' ,
+      // 'u_profile': registration.first_name,
+      'u_name': registration.u_name,
+      'u_email': registration.u_email,
+      'u_contactno': registration.u_contactno,
+      'u_psw': registration.u_psw,
+      // 'u_address':registration.address,
+      // 'country':registration.country,
+      // 'state':registration.state,
+      // 'city':registration.city,
+    };
     print("Insert data ");
-    //print(data);
+    print(data);
     final Response response = await post(
       Uri.parse(apiUrl + "/Reg_api.php"),
       body: {
