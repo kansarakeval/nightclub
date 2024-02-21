371713a2b5ee784ed43c32453cb7cf74d8efb83d
Jeniss09 <jenishmevada1@gmail.com>
2024-02-21 17:14:25 +0530

    new 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nightclub/utils/app_routes.dart';
 
-void main(){
-runApp(
- MaterialApp(
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(useMaterial3: true),
-  routes: app_Routes,
- )
-);
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(useMaterial3: true),
+    routes: app_Routes,
+  ));
 }
--- lib/screen/home/view/home_screen.dart
+++ lib/screen/home/view/home_screen.dart
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
               width: MediaQuery.sizeOf(context).width,
               decoration: BoxDecoration(
                 color: Colors.blue.shade800,
-                borderRadius: BorderRadius.only(
+                borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(20),
                   bottomRight: Radius.circular(20),
                 ),
@@ -37,15 +37,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CircleAvatar(
+                      const CircleAvatar(
                         radius: 30,
                         backgroundColor: Colors.white,
                         backgroundImage: AssetImage("assets/img/logo.png"),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 10,
                       ),
-                      Column(
+                      const Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
@@ -58,70 +58,153 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ],
                       ),
-                      Spacer(),
+                      const Spacer(),
                       IconButton(
                           onPressed: () {},
-                          icon: Icon(
+                          icon: const Icon(
                             Icons.notifications_rounded,
                             color: Colors.white,
                           ))
                     ],
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
-                  SearchBar(
-                      leading: Icon(Icons.search),
-                      hintText: "Search",
-                      trailing: [Icon(Icons.tune)]),
-                  SizedBox(
+                  const SearchBar(
+                    leading: Icon(Icons.search),
+                    hintText: "Search",
+                    trailing: [
+                      Icon(Icons.tune),
+                    ],
+                  ),
+                  const SizedBox(
                     height: 10,
                   ),
                   Container(
                     width: MediaQuery.sizeOf(context).width * 0.90,
                     height: MediaQuery.sizeOf(context).height * 0.50,
-                    padding: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                        color: Colors.red,
+                      color: Colors.white,
+                        boxShadow:  const [BoxShadow(color: Colors.grey,spreadRadius: 0.2,blurRadius: 2,)],
                         borderRadius: BorderRadius.circular(5)),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Stack(
-                            alignment: Alignment.bottomCenter,
-                            children: [
-                              Image.asset(
-                                "assets/img/singer1.jpg",
-                                height: 230,
-                              ),
-                              Container(
-                                height: 44,
-                                width: MediaQuery.sizeOf(context).width*0.84,
-                                padding: const EdgeInsets.only(left: 20),
-                                decoration: BoxDecoration(
+                        Center(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Stack(
+                              alignment: Alignment.bottomCenter,
+                              children: [
+                                Image.asset(
+                                  "assets/img/singer1.jpg",
+                                  height: 230,
+                                ),
+                                // Container(
+                                //   height: 15,
+                                //   width: 70,
+                                //   decoration: BoxDecoration(
+                                //     borderRadius: BorderRadius.circular(2),
+                                //     color: Colors.blueAccent.shade700,
+                                //   ),
+                                //   child: const Center(
+                                //     child: Text(
+                                //       "Singing"
+                                //     ),
+                                //   ),
+                                // ),
+                                Container(
+                                  height: 44,
+                                  width:
+                                      MediaQuery.sizeOf(context).width * 0.84,
+                                  padding: const EdgeInsets.only(left: 20),
+                                  decoration: BoxDecoration(
                                     color: Colors.blue.withOpacity(0.2),
                                     borderRadius: const BorderRadius.only(
-                                        bottomRight: Radius.circular(10),
-                                        bottomLeft: Radius.circular(10))),
-                                child: const Row(
-                                  children: [
-                                    Icon(Icons.thumb_up_alt_outlined,color: Colors.white,size: 17,),
-                                    SizedBox(width: 10,),
-                                    Text("231",style: TextStyle(color: Colors.white,fontSize: 13),),
-                                    SizedBox(width: 20,),
-                                    Icon(Icons.comment_outlined,color: Colors.white,size: 17,),
-                                    SizedBox(width: 10,),
-                                    Text("441",style: TextStyle(color: Colors.white,fontSize: 13),),
-                                    SizedBox(width: 20,),
-                                    Icon(Icons.share_outlined,color: Colors.white,size: 17,),
-                                    SizedBox(width: 10,),
-                                    Text("631",style: TextStyle(color: Colors.white,fontSize: 13),),
-                                  ],
+                                      bottomRight: Radius.circular(10),
+                                      bottomLeft: Radius.circular(10),
+                                    ),
+                                  ),
+                                  child: const Row(
+                                    children: [
+                                      Icon(
+                                        Icons.thumb_up_alt_outlined,
+                                        color: Colors.white,
+                                        size: 17,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "231",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 13),
+                                      ),
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Icon(
+                                        Icons.comment_outlined,
+                                        color: Colors.white,
+                                        size: 17,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "441",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 13),
+                                      ),
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Icon(
+                                        Icons.share_outlined,
+                                        color: Colors.white,
+                                        size: 17,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "631",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 13),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "Singing trends",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            const Text("New York",),
+                            const Spacer(),
+                            Icon(Icons.star,color: Colors.blue.shade800,size: 18,),
+                            Icon(Icons.star,color: Colors.blue.shade800,size: 18,),
+                            Icon(Icons.star,color: Colors.blue.shade800,size: 18,),
+                            Icon(Icons.star,color: Colors.blue.shade800,size: 18,),
+                            Icon(Icons.star_half,color: Colors.blue.shade800,size: 18,),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset("assets/img/singer2.jpg",height: 50,),
+                          ],
                         ),
                       ],
                     ),
